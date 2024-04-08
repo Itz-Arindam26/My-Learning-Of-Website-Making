fe9968d099883aed22d3877fbf6158482e1e72c0
Itz-Arindam26 <samantaarindam3434@gmail.com>
2024-04-08 17:25:28 +0530

    Video 47

---
+++ Video 47/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World")
+// In this folder , we will know about the conditional statements used in .js
+let _age = 0;
+// first we will see about if else conditionals 
+
+if (_age>18) {
+    console.log("You can drive...");
+}
+else{
+    console.log("You cannot drive...");
+}
