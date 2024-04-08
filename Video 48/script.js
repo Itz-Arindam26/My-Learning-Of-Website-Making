02d7ff77c87d7f0ad1af482fad111a2ff188b36a
Itz-Arindam26 <samantaarindam3434@gmail.com>
2024-04-08 19:39:41 +0530

    Video 48

---
+++ Video 48/script.js
@@ -0,0 +1,23 @@
+console.log("This is the folder in the loops");
+
+// for loop
+for (let a =0; a <=100; a++){
+    console.log(a);
+}
+// for of loop
+for (const s of 'ASamanta') {
+    console.log(s);
+}
+// while loop
+let a = 1;
+while(a<=50 ){
+    console.log(a);
+    a++;
+};
+// do-while loop
+console.log("Do-while loop")
+let x = 0;
+do {
+    console.log(x)
+    x++;
+} while (x<=100);
