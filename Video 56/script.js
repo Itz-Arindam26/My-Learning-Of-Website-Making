c726dd3714b1f0ebfa7cd3043bcf942b88280ac8
Itz-Arindam26 <samantaarindam3434@gmail.com>
2024-04-11 17:57:26 +0530

    Video 56

---
+++ Video 56/script.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+
+
+// document.body.childNodes[1].childNodes
+
+// let contme = document.body.childNodes[1]
+// see this command in console
+
+
+// contme.childNodes
+
+// contme.childNodes[1]
+
+// contme.firstElementChild
+
+// contme.lastElementChild
+// contme.lastElementChild.style.backgroundColor = 'light green';
+
+// contme.lastElementChild.style.backgroundColor = "light brown";
+
+// contme.lastElementChild.style.backgroundColor = "red";
+
+// contme.lastElementChild.style.color = "white";
+
+// contme.firstElementChild.parentElement
