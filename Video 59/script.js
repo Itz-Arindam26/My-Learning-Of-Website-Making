0580de922b3255e6da70bbad09547a26db89a364
Itz-Arindam26 <samantaarindam3434@gmail.com>
2024-04-14 13:38:02 +0530

    Video 59

---
+++ Video 59/script.js
@@ -0,0 +1,8 @@
+// this is used to fetch the code in a class
+document.querySelector(".box").innerHTML;
+document.querySelector(".container").innerHTML;
+// this is used to fetch text in a class
+document.querySelector(".box").innerText;
+document.querySelector(".container").innerText;
+Document.querySelector(".container").outerHTML;
+console.log(document.querySelector(".container").outerHTML);
